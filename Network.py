788e85e090d3ae4f3ba61285f69dea83b3d5f5b8
Curtis Black <curtis.w.black@gmail.com>
2017-07-16 18:05:46 +1000

    Updated networking.

--- Network.py
+++ Network.py
@@ -2,6 +2,7 @@ import os
 import time
 import socket
 import logging
+import atexit
 
 class Network:
     def __init__(self):
@@ -12,9 +13,16 @@ class Network:
                       "b8:27:eb:c4:25:de": "R2D2",
                       "b8:27:eb:91:70:8b": "R2D2" }
         self.IPs = { "R2D2": None, "BB8": None }
-        self.outgoing = socket.socket(socket.AF_INET, socket.DGRAM)
+        self.outgoing = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.messages = { "R2D2": [], "BB8": [] }
         self.port = 5000
+        atexit.register(self.Exit)
+
+    def Exit(self):
+        self.Send("R2D2", "bye")
+        self.outgoing.close()
+        if self.incoming != None:
+            self.incoming.close()
 
     def Update(self):
         t = time.time()
@@ -33,27 +41,33 @@ class Network:
                 if self.incoming != None:
                     self.incoming.close()
                 if self.IP != None:
-                    print "opening port on", self.IP
+                    print "Opening port", self.port, "on", self.IP
                     self.incoming = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                     self.incoming.bind((self.IP, self.port))
-                    for i in range(255):
-                        self.outgoing.sentto("hi", ("192.168.0." + str(i), self.port))
+                    for i in range(1, 255):
+                        self.outgoing.sendto("hi", ("192.168.0." + str(i), self.port))
                 else:
                     self.incoming = None
 
         if self.incoming != None:
             try:
-                data, (address, port) = self.incoming.recvfrom(1024)
-                mac = os.popen("arp -na | grep " + address + " | cut -d' ' -f4").read().split("\n")[0]
-                if mac in self.MACs:
-                    droid = self.MACs[mac]
-                    if self.IPs[droid] != address:
-                        print "Discovered", droid
-                        self.IPs[droid] = address
-                    if data == "hi":
-                        self.Send(droid, "return")
+                data, (address, port) = self.incoming.recvfrom(1024, socket.MSG_DONTWAIT)
+                if address != self.IP:
+                    mac = os.popen("arp -na | grep " + address + " | cut -d' ' -f4").read().split("\n")[0]
+                    if mac in self.MACs:
+                        droid = self.MACs[mac]
+                        if self.IPs[droid] != address:
+                            print "Discovered", droid
+                            self.IPs[droid] = address
+                        if data == "hi":
+                            self.Send(droid, "return")
+                        elif data == "bye":
+                            print "Lost", droid
+                            self.IPs[droid] = None
+                        else:
+                            self.messages[droid].append(data)
                     else:
-                        self.messages[droid].append(data)
+                        print "Received", data, "from", address
             except socket.error:
                 pass
 
